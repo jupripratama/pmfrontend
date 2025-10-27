77200e3a17e1b83b055c3b28a9aa95b36920b539
jupri eka pratama <jupri.eka@mkncorp.com>
2025-10-27 12:48:50 +0800

    fix: clean valid vercel

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist', // pastikan ini sama dengan vercel output
+  },
   server: {
     proxy: {
       '/api': {
         target: 'http://localhost:5116',
         changeOrigin: true,
         secure: false,
-      }
-    }
-  }
+      },
+    },
+  },
 })
